c1e05f64ba07f173974e64a9c25a084accf34366
Scot Nielson <scot@MacBook-Pro-384.home>
2024-11-30 17:40:02 -0700

    background, scripts

---
+++ Assets/Scripts/ButtonBehavior.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonBehavior : MonoBehaviour
+{
+    private int n;
+    public void OnButtonClick() {
+        n++;
+        Debug.Log($"Button clicked {n} times.");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
